48f8eabb5c3f8d23e2d5b19980956bbfd16c0e9d
Valentin Louf <valentin.louf@bom.gov.au>
2020-03-18 09:32:58 +1100

    Uodated backbone

--- scripts/national_archive.py
+++ scripts/national_archive.py
@@ -1,6 +1,8 @@
+import gc
 import os
 import glob
 import zipfile
+import argparse
 import datetime
 import warnings
 import traceback
@@ -30,13 +32,21 @@ def extract_zip(inzip, path):
 
 
 def remove(flist):
+    flist = [f for f in flist if f is not None]
     for f in flist:
-        if f is None:
-            continue
         try:
             os.remove(f)
         except FileNotFoundError:
             pass
+    return None
+
+
+def savedata(rslt_list, path):
+
+    df = pd.concat(rslt_list)
+    outfilename = os.path.join(path, 'test.csv')
+    df.to_csv(outfilename)
+
     return None
 
 
@@ -50,6 +60,24 @@ def buffer(infile):
 
 
 def main():
+    zipdir = '/scratch/kl02/vhl548/unzipdir/'
+    outpath = f'/scratch/kl02/vhl548/solar_output/'
+
+    flist = sorted(glob.glob('/g/data/rq0/odim_archive/odim_pvol/02/2019/vol/*.zip'))
+
+    for zipfile in flist:
+        namelist = extract_zip(zipfile, path=zipdir)
+
+        bag = db.from_sequence(namelist).map(buffer)
+        rslt = bag.compute()
+        rslt = [r for r in rslt if r is not None]
+
+        if not len(rslt) == 0:
+            savedata(rslt, path=outpath)
+
+        remove(namelist)
+        del bag
+        gc.collect()
 
     return None
 
