f095eac6e99044968d21ef8926a2e6206bba1ff7
Valentin Louf <valentin.louf@bom.gov.au>
2020-03-18 09:51:28 +1100

    ADDED: Argument parser

--- scripts/national_archive.py
+++ scripts/national_archive.py
@@ -1,5 +1,6 @@
 import gc
 import os
+import sys
 import glob
 import zipfile
 import argparse
@@ -15,13 +16,13 @@ import suncal
 from suncal import SunNotFoundError
 
 
-def mkdir(path):
+def buffer(infile):
     try:
-        os.mkdir(path)
-    except FileExistsError:
-        pass
+        rslt = suncal.sunpos_reflectivity(infile)
+    except SunNotFoundError:
+        return None
 
-    return None
+    return rslt
 
 
 def extract_zip(inzip, path):
@@ -29,6 +30,24 @@ def extract_zip(inzip, path):
         zid.extractall(path=path)
         namelist = [os.path.join(path, f) for f in zid.namelist()]
     return namelist
+
+
+def get_radar_archive_file(date):
+    datestr = date.strftime('%Y%m%d')
+    file = f"/g/data/rq0/odim_archive/odim_pvol/{RID}/{date.year}/vol/{RID}_{datestr}.pvol.zip"
+    if not os.path.exists(file):
+        return None
+
+    return file
+
+
+def mkdir(path):
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        pass
+
+    return None
 
 
 def remove(flist):
@@ -43,38 +62,32 @@ def remove(flist):
 
 def savedata(rslt_list, path):
 
-    df = pd.concat(rslt_list)
-    outfilename = os.path.join(path, 'test.csv')
+    df = pd.concat(rslt_list).reset_index()
+    dtime = df.time[0].strftime('%Y%m%d')
+    outfilename = os.path.join(path, f'suncal.{RID}.{dtime}.csv')
     df.to_csv(outfilename)
 
     return None
 
 
-def buffer(infile):
-    try:
-        rslt = suncal.sunpos_reflectivity(infile)
-    except SunNotFoundError:
-        return None
-
-    return rslt
-
-
 def main():
-    zipdir = '/scratch/kl02/vhl548/unzipdir/'
-    outpath = f'/scratch/kl02/vhl548/solar_output/'
+    for date in pd.date_range(START_DATE, END_DATE):
+        zipfile = get_radar_archive_file(date)
+        if zipfile is None:
+            print(f'No file found for date {date}.')
+            continue
 
-    flist = sorted(glob.glob('/g/data/rq0/odim_archive/odim_pvol/02/2019/vol/*.zip'))
-
-    for zipfile in flist:
-        namelist = extract_zip(zipfile, path=zipdir)
-
+        namelist = extract_zip(zipfile, path=ZIPDIR)
         bag = db.from_sequence(namelist).map(buffer)
         rslt = bag.compute()
         rslt = [r for r in rslt if r is not None]
 
-        if not len(rslt) == 0:
-            savedata(rslt, path=outpath)
+        if len(rslt) == 0:
+            print(f'No results for date {date}.')
+        else:
+            savedata(rslt, path=OUTPATH)
 
+        # Removing unzipped files, collecting memory garbage.
         remove(namelist)
         del bag
         gc.collect()
@@ -83,4 +96,54 @@ def main():
 
 
 if __name__ == "__main__":
+    parser_description = "Solar calibration of radar in the National radar archive."
+    parser = argparse.ArgumentParser(description=parser_description)
+    parser.add_argument(
+        "-r",
+        "--rid",
+        dest="rid",
+        type=int,
+        required=True,
+        help="Radar ID number.")
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="output",
+        default='/scratch/kl02/vhl548/solar_output/',
+        type=str,
+        help="Output directory")
+    parser.add_argument(
+        '-s',
+        '--start-date',
+        dest='start_date',
+        default=None,
+        type=str,
+        help='Starting date.',
+        required=True)
+    parser.add_argument(
+        '-e',
+        '--end-date',
+        dest='end_date',
+        default=None,
+        type=str,
+        help='Ending date.',
+        required=True)
+
+    args = parser.parse_args()
+    RID = args.rid
+    START_DATE = args.start_date
+    END_DATE = args.end_date
+    OUTPATH = args.output
+    ZIPDIR = '/scratch/kl02/vhl548/unzipdir/'
+
+    try:
+        start = datetime.datetime.strptime(START_DATE, "%Y%m%d")
+        end = datetime.datetime.strptime(END_DATE, "%Y%m%d")
+        if start > end:
+            parser.error('End date older than start date.')
+        date_range = [start + datetime.timedelta(days=x) for x in range(0, (end - start).days + 1, )]
+    except ValueError:
+        parser.error('Invalid dates.')
+        sys.exit()
+
     main()
