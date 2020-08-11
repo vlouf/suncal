addc2b669f9deb3e39c6cf7b1eb647d660a2c58f
Valentin Louf <valentin.louf@bom.gov.au>
2020-08-11 14:48:22 +1000

    MODIF: pep8

--- scripts/national_archive.py
+++ scripts/national_archive.py
@@ -1,4 +1,4 @@
-'''
+"""
 Radar calibration using the Sun as reference. Processing the Australian
 National archive.
 
@@ -14,7 +14,7 @@ National archive.
     remove
     savedata
     main
-'''
+"""
 import gc
 import os
 import sys
@@ -36,7 +36,7 @@ from suncal import SunNotFoundError
 
 
 def buffer(infile):
-    '''
+    """
     Buffer function to catch and kill errors about missing Sun hit.
 
     Parameters:
@@ -48,13 +48,13 @@ def buffer(infile):
     ========
     rslt: pd.DataFrame
         Pandas dataframe with the results from the solar calibration code.
-    '''
+    """
     try:
         rslt = suncal.sunpos_reflectivity(infile)
     except SunNotFoundError:
         return None
     except Exception:
-        print(f'Problem with file {infile}.')
+        print(f"Problem with file {infile}.")
         traceback.print_exc()
         return None
 
@@ -62,15 +62,15 @@ def buffer(infile):
 
 
 def check_rid():
-    '''
+    """
     Check if the Radar ID provided exists.
-    '''
-    indir = f'/g/data/rq0/level_1/odim_pvol/{RID}'
+    """
+    indir = f"/g/data/rq0/level_1/odim_pvol/{RID}"
     return os.path.exists(indir)
 
 
 def extract_zip(inzip, path):
-    '''
+    """
     Extract content of a zipfile inside a given directory.
 
     Parameters:
@@ -84,7 +84,7 @@ def extract_zip(inzip, path):
     ========
     namelist: List
         List of files extracted from  the zip.
-    '''
+    """
     with zipfile.ZipFile(inzip) as zid:
         zid.extractall(path=path)
         namelist = [os.path.join(path, f) for f in zid.namelist()]
@@ -92,7 +92,7 @@ def extract_zip(inzip, path):
 
 
 def get_radar_archive_file(date):
-    '''
+    """
     Return the archive containing the radar file for a given radar ID and a
     given date.
 
@@ -105,9 +105,10 @@ def get_radar_archive_file(date):
     ========
     file: str
         Radar archive if it exists at the given date.
-    '''
-    datestr = date.strftime('%Y%m%d')
+    """
+    datestr = date.strftime("%Y%m%d")
     file = f"/g/data/rq0/level_1/odim_pvol/{RID}/{date.year}/vol/{RID}_{datestr}.pvol.zip"
+    
     if not os.path.exists(file):
         return None
 
@@ -115,9 +116,9 @@ def get_radar_archive_file(date):
 
 
 def mkdir(path):
-    '''
+    """
     Create the DIRECTORY(ies), if they do not already exist
-    '''
+    """
     try:
         os.mkdir(path)
     except FileExistsError:
@@ -127,9 +128,9 @@ def mkdir(path):
 
 
 def remove(flist):
-    '''
+    """
     Remove file if it exists.
-    '''
+    """
     flist = [f for f in flist if f is not None]
     for f in flist:
         try:
@@ -140,7 +141,7 @@ def remove(flist):
 
 
 def savedata(rslt_list, path):
-    '''
+    """
     Save the output data into a CSV file compatible with pandas.
 
     Parameters:
@@ -149,20 +150,20 @@ def savedata(rslt_list, path):
         List of solar calibration results for the given date.
     path: str
         Output directory.
-    '''
+    """
     df = pd.concat(rslt_list, ignore_index=True)
-    dtime = df.time[0].strftime('%Y%m%d')
-    year = df.time[0].strftime('%Y')
+    dtime = df.time[0].strftime("%Y%m%d")
+    year = df.time[0].strftime("%Y")
 
     path = os.path.join(path, RID)
     mkdir(path)
     path = os.path.join(path, year)
     mkdir(path)
 
-    outfilename = os.path.join(path, f'suncal.{RID}.{dtime}.csv')
-    df.to_csv(outfilename, float_format='%g')
-    print(crayons.green(f'{len(df)} solar hits on {dtime}.'))
-    print(crayons.green(f'Results saved in {outfilename}.'))
+    outfilename = os.path.join(path, f"suncal.{RID}.{dtime}.csv")
+    df.to_csv(outfilename, float_format="%g")
+    print(crayons.green(f"{len(df)} solar hits on {dtime}."))
+    print(crayons.green(f"Results saved in {outfilename}."))
 
     return None
 
@@ -171,17 +172,17 @@ def main(date_range):
     for date in date_range:
         zipfile = get_radar_archive_file(date)
         if zipfile is None:
-            print(crayons.red(f'No file found for date {date}.'))
+            print(crayons.red(f"No file found for date {date}."))
             continue
 
         namelist = extract_zip(zipfile, path=ZIPDIR)
-        print(crayons.yellow(f'{len(namelist)} files to process for {date}.'))
+        print(crayons.yellow(f"{len(namelist)} files to process for {date}."))
         bag = db.from_sequence(namelist).map(buffer)
         rslt = bag.compute()
         rslt = [r for r in rslt if r is not None]
 
         if len(rslt) == 0:
-            print(crayons.red(f'No results for date {date}.'))
+            print(crayons.red(f"No results for date {date}."))
         else:
             savedata(rslt, path=OUTPATH)
 
@@ -196,65 +197,62 @@ def main(date_range):
 if __name__ == "__main__":
     parser_description = "Solar calibration of radar in the National radar archive."
     parser = argparse.ArgumentParser(description=parser_description)
-    parser.add_argument(
-        "-r",
-        "--rid",
-        dest="rid",
-        type=int,
-        required=True,
-        help="Radar ID number.")
+    parser.add_argument("-r", "--rid", dest="rid", type=int, required=True, help="Radar ID number.")
     parser.add_argument(
         "-o",
         "--output",
         dest="output",
-        default='/scratch/kl02/vhl548/solar_output/',
+        default="/scratch/kl02/vhl548/solar_output/",
         type=str,
-        help="Output directory")
+        help="Output directory",
+    )
     parser.add_argument(
-        '-s',
-        '--start-date',
-        dest='start_date',
+        "-s",
+        "--start-date",
+        dest="start_date",
         default=None,
         type=str,
-        help='Starting date.',
-        required=True)
+        help="Starting date.",
+        required=True,
+    )
     parser.add_argument(
-        '-e',
-        '--end-date',
-        dest='end_date',
+        "-e",
+        "--end-date",
+        dest="end_date",
         default=None,
         type=str,
-        help='Ending date.',
-        required=True)
+        help="Ending date.",
+        required=True,
+    )
 
     args = parser.parse_args()
     RID = f"{args.rid:02}"
     START_DATE = args.start_date
     END_DATE = args.end_date
     OUTPATH = args.output
-    ZIPDIR = '/scratch/kl02/vhl548/unzipdir/'
+    ZIPDIR = "/scratch/kl02/vhl548/unzipdir/"
 
     if not check_rid():
-        parser.error('Invalid Radar ID.')
+        parser.error("Invalid Radar ID.")
         sys.exit()
 
     try:
         start = datetime.datetime.strptime(START_DATE, "%Y%m%d")
         end = datetime.datetime.strptime(END_DATE, "%Y%m%d")
         if start > end:
-            parser.error('End date older than start date.')
-        date_range = [start + datetime.timedelta(days=x) for x in range(0, (end - start).days + 1, )]
+            parser.error("End date older than start date.")
+        date_range = [start + datetime.timedelta(days=x) for x in range(0, (end - start).days + 1,)]
     except ValueError:
-        parser.error('Invalid dates.')
+        parser.error("Invalid dates.")
         sys.exit()
 
-    print(crayons.green(f'Processing sun calibration for radar {RID}.'))
-    print(crayons.green(f'Between {START_DATE} and {END_DATE}.'))
-    print(crayons.green(f'Data will be saved in {OUTPATH}.'))
+    print(crayons.green(f"Processing sun calibration for radar {RID}."))
+    print(crayons.green(f"Between {START_DATE} and {END_DATE}."))
+    print(crayons.green(f"Data will be saved in {OUTPATH}."))
 
     tick = time.time()
     with warnings.catch_warnings():
-        warnings.simplefilter('ignore')
+        warnings.simplefilter("ignore")
         main(date_range)
     tock = time.time()
-    print(crayons.magenta(f'Process finished in {tock - tick:0.2}s.'))
+    print(crayons.magenta(f"Process finished in {tock - tick:0.2}s."))
